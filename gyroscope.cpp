206b747f86904fcbcfa419a4d5abbc94be2a96cc
ErenGunduzz <akineren1121@hotmail.com>
2024-08-09 16:43:17 +0300

    last update format

--- gyroscope.cpp
+++ gyroscope.cpp
@@ -6,8 +6,8 @@
  */
 
 #include "gyroscope.hpp"
-#include <iostream>
 #include <fcntl.h>
+#include <iostream>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
@@ -16,95 +16,143 @@ using namespace std;
 
 namespace OBU {
 
-const uint8_t GyroOperations::SENSOR_ADDR = 0x68;
-const string GyroOperations::I2C_BUS = "/dev/i2c-1";
-const uint8_t GyroOperations::SENSOR_REG_GYRO_XOUT_H = 0x43; //0x1D for ITG3205
-const uint8_t GyroOperations::SENSOR_REG_POWER_CTL = 0x6B; // 0x6B is the power management register
-// const uint8_t GyroOperations::SENSOR_REG_DATAX0 = 0x32;
-const uint8_t GyroOperations::SENSOR_MEASURE_MODE = 0x00;
-const int GyroOperations::NO_FILE_FD = -1;
-const int GyroOperations::FILE_OP_VALUE = 0;
-const int GyroOperations::GYRO_WR_REG = 2;
-const int GyroOperations::SHIFT_VALUE = 8;
-const size_t GyroOperations::BUFFER_SIZE = 6;
-const size_t GyroOperations::WR_REG_BUFFER_SIZE = 1;
-const int GyroOperations::X_AXIS_START_VALUE = 0;
-const int GyroOperations::Y_AXIS_START_VALUE = 0;
-const int GyroOperations::Z_AXIS_START_VALUE = 0;
-const int GyroOperations::BUFFER_START_VALUE = 0;
+    uint8_t const GyroOperations::SENSOR_ADDR = 0x68;
+    string const GyroOperations::I2C_BUS = "/dev/i2c-1";
+    uint8_t const GyroOperations::SENSOR_REG_GYRO_XOUT_H = 0x43; //0x1D for ITG3205
+    uint8_t const GyroOperations::SENSOR_REG_POWER_CTL = 0x6B; // 0x6B is the power management register
+    // const uint8_t const GyroOperations::SENSOR_REG_DATAX0 = 0x32;
+    uint8_t const GyroOperations::SENSOR_MEASURE_MODE = 0x00;
+    int32_t const GyroOperations::NO_FILE_FD = -1;
+    int32_t const GyroOperations::FILE_OP_VALUE = 0;
+    size_t const GyroOperations::WR_REG_BUFFER = 2;
+    int32_t const GyroOperations::SHIFT_VALUE = 8;
+    size_t const GyroOperations::BUFFER_SIZE = 6;
+    size_t const GyroOperations::WR_REG_BUFFER_SIZE = 1;
+    int32_t const GyroOperations::X_AXIS_START_VALUE = 0;
+    int32_t const GyroOperations::Y_AXIS_START_VALUE = 0;
+    int32_t const GyroOperations::Z_AXIS_START_VALUE = 0;
+    int32_t const GyroOperations::BUFFER_START_VALUE = 0;
 
-GyroOperations::GyroOperations() {
-    this->device = I2C_BUS;
-    mFileDescriptor = NO_FILE_FD;
-}
-
-GyroOperations::~GyroOperations() {
-    if (mFileDescriptor >= FILE_OP_VALUE) {
-        close(mFileDescriptor);
+    GyroOperations::GyroOperations() {
+        this->device = I2C_BUS;
+        mFileDescriptor = NO_FILE_FD;
     }
-}
 
-GyroError GyroOperations::initialize() {
-    GyroError result = GyroError::SUCCESS;
-
-    mFileDescriptor = open(device.c_str(), O_RDWR);
-    if (mFileDescriptor < FILE_OP_VALUE) {
-        cerr << "Failed to open I2C bus!" << endl;
-        result = GyroError::I2C_OPEN_ERR;
-    } else if (ioctl(mFileDescriptor, I2C_SLAVE, SENSOR_ADDR) < FILE_OP_VALUE) {
-        cerr << "Failed to set I2C address for sensor." << endl;
-        result = GyroError::I2C_ADDR_SET_ERR;
-    } else {
-        // Wake up the sensor as it starts in sleep mode
-        if (writeRegister(SENSOR_REG_POWER_CTL, SENSOR_MEASURE_MODE) != GyroError::SUCCESS) { // SENSOR_REG_POWER_CTL is the power management register
-            cerr << "Failed to wake up sensor." << endl;
-            result = GyroError::SENSOR_WAKE_FAIL;
+    GyroOperations::~GyroOperations() {
+        if (mFileDescriptor >= FILE_OP_VALUE) {
+            close(mFileDescriptor);
         }
     }
-    return result;
-}
 
-GyroError GyroOperations::readGyro(double& x, double& y, double& z) {
-    uint8_t buffer[BUFFER_SIZE] = {BUFFER_START_VALUE};
-
-    GyroError result = readRegisters(SENSOR_REG_GYRO_XOUT_H, buffer, BUFFER_SIZE);
-    if (result == GyroError::SUCCESS) {
-        x = ((buffer[0] << SHIFT_VALUE) | buffer[1]) / scaleFactor;
-        y = ((buffer[2] << SHIFT_VALUE) | buffer[3]) / scaleFactor;
-        z = ((buffer[4] << SHIFT_VALUE) | buffer[5]) / scaleFactor;
-    }
-    return result;
-}
-
-GyroError GyroOperations::writeRegister(uint8_t reg, uint8_t value) {
-    uint8_t buffer[GYRO_WR_REG] = {reg, value};
-    GyroError result = GyroError::SUCCESS;
-
-    if (write(mFileDescriptor, buffer, GYRO_WR_REG) != GYRO_WR_REG) {
-        result = GyroError::I2C_WRITE_ERR;
-    }
-    return result;
-}
-
-GyroError GyroOperations::readRegisters(uint8_t reg, uint8_t* buffer, size_t length) {
-    GyroError result = GyroError::SUCCESS;
-    if (write(mFileDescriptor, &reg, WR_REG_BUFFER_SIZE) != WR_REG_BUFFER_SIZE) {
-        result = GyroError::I2C_WRITE_ERR;
-    }
-    if (read(mFileDescriptor, buffer, length) != static_cast<int>(length)) {
-        result = GyroError::I2C_READ_ERR;
-    }
-    return GyroError::SUCCESS;
-}
-
-GyroscopeData GyroOperations::getGyroData() {
-    GyroscopeData data = {X_AXIS_START_VALUE, Y_AXIS_START_VALUE, Z_AXIS_START_VALUE};
-
-    if (readGyro(data.x, data.y, data.z) != GyroError::SUCCESS) {
-        cerr << "Failed to read gyroscope data!" << endl;
+    /**
+     * @brief Getter for device.
+     * @return The value of device.
+     */
+    string GyroOperations::getDeviceName() const{
+        return (device);
     }
 
-    return data;
-}
+    /**
+     * @brief Setter for device.
+     * @param value The value to set for device.
+     */
+    void GyroOperations::setDeviceName(std::string device) {
+        this->device = move(device);
+    }
+
+    /**
+     * @brief Getter for mFileDescriptor.
+     * @return The value of mFileDescriptor.
+     */
+    int32_t GyroOperations::getFileDescriptor() const {
+        return (mFileDescriptor);
+    }
+
+    /**
+     * @brief Setter for device.
+     * @param value The value to set for device.
+     */
+    void GyroOperations::setFileDescriptor(int32_t fileDescriptor) {
+        mFileDescriptor = fileDescriptor;
+    }
+
+    /**
+     * @brief Getter for scaleFactor.
+     * @return The value of scaleFactor.
+     */
+    double GyroOperations::getScaleFactor() const {
+        return (scaleFactor);
+    }
+
+    /**
+     * @brief Setter for scaleFactor.
+     * @param value The value to set for scaleFactor.
+     */
+    void GyroOperations::setScaleFactor(double factor) {
+        scaleFactor = factor;
+    }
+
+    GyroError GyroOperations::initialize() {
+        GyroError result = GyroError::SUCCESS;
+
+        mFileDescriptor = open(device.c_str(), O_RDWR);
+        if (mFileDescriptor < FILE_OP_VALUE) {
+            cerr << "Failed to open I2C bus!" << endl;
+            result = GyroError::I2C_OPEN_ERR;
+        } else if (ioctl(mFileDescriptor, I2C_SLAVE, SENSOR_ADDR) < FILE_OP_VALUE) {
+            cerr << "Failed to set I2C address for sensor." << endl;
+            result = GyroError::I2C_ADDR_SET_ERR;
+        } else {
+            // Wake up the sensor as it starts in sleep mode
+            if (writeRegister(SENSOR_REG_POWER_CTL, SENSOR_MEASURE_MODE) != GyroError::SUCCESS) { // SENSOR_REG_POWER_CTL is the power management register
+                cerr << "Failed to wake up sensor." << endl;
+                result = GyroError::SENSOR_WAKE_FAIL;
+            }
+        }
+        return (result);
+    }
+
+    GyroError GyroOperations::readGyro(double& x, double& y, double& z) {
+        uint8_t buffer[BUFFER_SIZE] = {BUFFER_START_VALUE};
+
+        GyroError result = readRegisters(SENSOR_REG_GYRO_XOUT_H, buffer, BUFFER_SIZE);
+        if (result == GyroError::SUCCESS) {
+            x = ((buffer[0] << SHIFT_VALUE) | buffer[1]) / scaleFactor;
+            y = ((buffer[2] << SHIFT_VALUE) | buffer[3]) / scaleFactor;
+            z = ((buffer[4] << SHIFT_VALUE) | buffer[5]) / scaleFactor;
+        }
+        return (result);
+    }
+
+    GyroError GyroOperations::writeRegister(uint8_t reg, uint8_t value) {
+        uint8_t buffer[WR_REG_BUFFER] = {reg, value};
+        GyroError result = GyroError::SUCCESS;
+
+        if (write(mFileDescriptor, buffer, WR_REG_BUFFER) != WR_REG_BUFFER) {
+            result = GyroError::I2C_WRITE_ERR;
+        }
+        return (result);
+    }
+
+    GyroError GyroOperations::readRegisters(uint8_t reg, uint8_t* buffer, size_t length) {
+        GyroError result = GyroError::SUCCESS;
+        if (write(mFileDescriptor, &reg, WR_REG_BUFFER_SIZE) != WR_REG_BUFFER_SIZE) {
+            result = GyroError::I2C_WRITE_ERR;
+        }
+        if (read(mFileDescriptor, buffer, length) != static_cast<int>(length)) {
+            result = GyroError::I2C_READ_ERR;
+        }
+        return (GyroError::SUCCESS);
+    }
+
+    GyroscopeData GyroOperations::getGyroData() {
+        GyroscopeData data = {X_AXIS_START_VALUE, Y_AXIS_START_VALUE, Z_AXIS_START_VALUE};
+
+        if (readGyro(data.x, data.y, data.z) != GyroError::SUCCESS) {
+            cerr << "Failed to read gyroscope data!" << endl;
+        }
+
+        return (data);
+    }
 
 } // namespace OBU
--- gyroscope.hpp
+++ gyroscope.hpp
@@ -18,110 +18,146 @@ namespace OBU {
 
     using namespace std;
 
-/**
- * @enum GyroError
- * @brief Enumeration of return values for function calls
- */
-enum class GyroError {
-    SUCCESS,          /**< Operation completed successfully */
-    I2C_OPEN_ERR,     /**< Error opening I2C bus */
-    I2C_ADDR_SET_ERR, /**< Error setting I2C slave address */
-    SENSOR_WAKE_FAIL, /**< Error setting power mode */
-    I2C_WRITE_ERR,
-    I2C_READ_ERR
-};
-
-/**
- * @struct GyroscopeData
- * @brief Struct to store gyroscope data for X, Y, Z axes
- */
-struct GyroscopeData {
-    double x;
-    double y;
-    double z;
-};
-
-/**
- * @class GyroOperations
- * @brief Class for managing gyroscope operations
- */
-class GyroOperations {
-public:
+    /**
+     * @enum GyroError
+     * @brief Enumeration of return values for function calls
+     */
+    enum class GyroError {
+        SUCCESS,          /**< Operation completed successfully */
+        I2C_OPEN_ERR,     /**< Error opening I2C bus */
+        I2C_ADDR_SET_ERR, /**< Error setting I2C slave address */
+        SENSOR_WAKE_FAIL, /**< Error setting power mode */
+        I2C_WRITE_ERR,
+        I2C_READ_ERR
+    };
 
     /**
-     * @brief Constructor for GyroOperations class
-     * @details There is no connection at the beginning.
+     * @struct GyroscopeData
+     * @brief Struct to store gyroscope data for X, Y, Z axes
      */
-    GyroOperations();
+    struct GyroscopeData {
+        double x;
+        double y;
+        double z;
+    };
 
     /**
-     * @brief Destructor for GyroOperations class
-     * @details Closes I2C communication after the end of operations.
+     * @class GyroOperations
+     * @brief Class for managing gyroscope operations
      */
-    ~GyroOperations();
+    class GyroOperations {
+    public:
 
-    /**
-     * @brief Initialize I2C communication
-     * @details Tries to start I2C communication process. If it is not successful, other processes will not be started.
-     * @return GyroError::SUCCESS or GyroError::I2C_OPEN_ERR if I2C opening fails.
-     */
-    GyroError initialize();
+        /**
+         * @brief Constructor for GyroOperations class
+         * @details There is no connection at the beginning.
+         */
+        GyroOperations();
 
-    /**
-     * @brief Read gyroscope data from sensor
-     * @details Reads the X, Y, and Z axis data as degree per second from the gyroscope sensor using the I2C interface.
-     * @return GyroError status of the operation.
-     */
-    GyroscopeData getGyroData();
+        /**
+         * @brief Destructor for GyroOperations class
+         * @details Closes I2C communication after the end of operations.
+         */
+        ~GyroOperations();
 
-private:
-    string device;                                      /**< I2C device file */
-    int mFileDescriptor;                                /**< File descriptor for I2C communication */
-    static const uint8_t SENSOR_ADDR;                   /**< I2C address of sensor */
-    static const string I2C_BUS;                        /**< I2C bus device file */
-    static const uint8_t SENSOR_REG_GYRO_XOUT_H;        /**< Register address for gyro X-axis high byte output */
-    static const uint8_t SENSOR_REG_POWER_CTL;
-    static const uint8_t SENSOR_REG_DATAX0;
-    static const uint8_t SENSOR_MEASURE_MODE;
-    static const int NO_FILE_FD;
-    static const int FILE_OP_VALUE;
-    static const int GYRO_WR_REG;
-    static const int SHIFT_VALUE;
-    static const size_t BUFFER_SIZE;
-    static const size_t WR_REG_BUFFER_SIZE;
-    static const int X_AXIS_START_VALUE;
-    static const int Y_AXIS_START_VALUE;
-    static const int Z_AXIS_START_VALUE;
-    static const int BUFFER_START_VALUE;
+        /**
+         * @brief Getter for device.
+         * @return The value of device.
+         */
+        string getDeviceName() const;
 
-    /**
-     * @brief Reads gyroscope data from the sensor
-     * @param x Reference to store X-axis data
-     * @param y Reference to store Y-axis data
-     * @param z Reference to store Z-axis data
-     * @return GyroError status of the operation
-     */
-   GyroError readGyro(double& x, double& y, double& z);
+        /**
+         * @brief Setter for device.
+         * @param value The value to set for device.
+         */
+        void setDeviceName(string device);
 
-    /**
-     * @brief Writes a value to a gyroscope register
-     * @param reg Register address
-     * @param value Value to write
-     * @return GyroError status of the operation
-     */
-    GyroError writeRegister(uint8_t reg, uint8_t value);
+        /**
+         * @brief Getter for mFileDescriptor.
+         * @return The value of mFileDescriptor.
+         */
+        int32_t getFileDescriptor() const;
 
-    /**
-     * @brief Reads values from gyroscope registers
-     * @param reg Register address
-     * @param buffer Buffer to store the read data
-     * @param length Number of bytes to read
-     * @return GyroError status of the operation
-     */
-    GyroError readRegisters(uint8_t reg, uint8_t* buffer, size_t length);
+        /**
+         * @brief Setter for device.
+         * @param value The value to set for device.
+         */
+        void setFileDescriptor(int mFileDescriptor);
 
-    double scaleFactor = 131.0; /**< Scale factor for MPU6050 for converting raw data to degrees per second */
-};
+        /**
+         * @brief Getter for scaleFactor.
+         * @return The value of scaleFactor.
+         */
+        double getScaleFactor() const;
+
+        /**
+         * @brief Setter for scaleFactor.
+         * @param value The value to set for scaleFactor.
+         */
+        void setScaleFactor(double scaleFactor);
+
+        /**
+         * @brief Initialize I2C communication
+         * @details Tries to start I2C communication process. If it is not successful, other processes will not be started.
+         * @return GyroError::SUCCESS or GyroError::I2C_OPEN_ERR if I2C opening fails.
+         */
+        GyroError initialize();
+
+        /**
+         * @brief Read gyroscope data from sensor
+         * @details Reads the X, Y, and Z axis data as degree per second from the gyroscope sensor using the I2C interface.
+         * @return GyroError status of the operation.
+         */
+        GyroscopeData getGyroData();
+
+    private:
+        string device;                                      /**< I2C device file */
+        int32_t mFileDescriptor;                            /**< File descriptor for I2C communication */
+        double scaleFactor;                                 /**< Scale factor for MPU6050 for converting 
+        raw data to degrees per second 131.0*/
+        static uint8_t const SENSOR_ADDR;                   /**< I2C address of sensor */
+        static string const I2C_BUS;                        /**< I2C bus device file */
+        static uint8_t const SENSOR_REG_GYRO_XOUT_H;        /**< Register address for gyro X-axis high byte output */
+        static uint8_t const SENSOR_REG_POWER_CTL;
+        // const uint8_t const GyroOperations::SENSOR_REG_DATAX0 = 0x32;
+        static uint8_t const SENSOR_MEASURE_MODE;
+        static int32_t const NO_FILE_FD;
+        static int32_t const FILE_OP_VALUE;
+        static size_t const WR_REG_BUFFER;
+        static int32_t const SHIFT_VALUE;
+        static size_t const BUFFER_SIZE;
+        static size_t const WR_REG_BUFFER_SIZE;
+        static int32_t const X_AXIS_START_VALUE;
+        static int32_t const Y_AXIS_START_VALUE;
+        static int32_t const Z_AXIS_START_VALUE;
+        static int32_t const BUFFER_START_VALUE;
+
+        /**
+         * @brief Reads gyroscope data from the sensor
+         * @param x Reference to store X-axis data
+         * @param y Reference to store Y-axis data
+         * @param z Reference to store Z-axis data
+         * @return GyroError status of the operation
+         */
+        GyroError readGyro(double& x, double& y, double& z);
+
+        /**
+         * @brief Writes a value to a gyroscope register
+         * @param reg Register address
+         * @param value Value to write
+         * @return GyroError status of the operation
+         */
+        GyroError writeRegister(uint8_t reg, uint8_t value);
+
+        /**
+         * @brief Reads values from gyroscope registers
+         * @param reg Register address
+         * @param buffer Buffer to store the read data
+         * @param length Number of bytes to read
+         * @return GyroError status of the operation
+         */
+        GyroError readRegisters(uint8_t reg, uint8_t* buffer, size_t length);
+    }; // class GyroOperations
 
 } // namespace OBU
 
